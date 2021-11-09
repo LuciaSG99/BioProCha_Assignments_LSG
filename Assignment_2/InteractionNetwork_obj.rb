a937201a0d58cf51d73ffaf8f464d7d9da80c491
Lucia Sanchez Gonzalez <lusangonz99@gmail.com>
2021-11-09 15:44:33 -0500

    Assignment 2

--- Assignment_2/InteractionNetwork_obj.rb
+++ Assignment_2/InteractionNetwork_obj.rb
@@ -1,40 +1,40 @@
+#Class that will contain the networks and their respective KEGG and GO annotations:
+
 class InteractionNetwork
-#contains the members of each network. 
 
   attr_accessor :gene_original #The object of the first gene used to search for interactions
-  attr_accessor :network 
-  attr_accessor :all_genes #hash_interactions
-  attr_accessor :go_annotations
-  attr_accessor :kegg_annotations
+  attr_accessor :network #attribute that will contain the members of the network
+  attr_accessor :all_genes #hash with all the gene objects
+  attr_accessor :go_annotations #attribute that will contain the KEGG annotations
+  attr_accessor :kegg_annotations #attribute that will contain the GO annotations
   require 'rest-client'
   require 'json'
   
-  def initialize(params)
-    @gene_original = params.fetch(:gene_original, nil)
+  def initialize(params = {}) #the values of the properties that the new object Interaction Network will have
+    @gene_original = params.fetch(:gene_original, nil) 
     @all_genes = params.fetch(:all_genes, Hash.new)
     @network = Array.new
-    recursive_function(@gene_original)
+    recursive_function(@gene_original) #applies the recursive function to obtain the network members
     @go_annotations = Hash.new
     @kegg_annotations = Hash.new
-    obtain_kegg_network
-    obtain_go_network
-    
+    obtain_kegg_network #applies the recursive function to obtain the KEGG annotations of the original genes involved in the network
+    obtain_go_network #applies the recursive function to obtain the GO annotations of the original genes involved in the network
+    @network = @network.sort_by{|obj| obj.agi_locus} #order the genes objects in the network by the AGI locus. 
   end
       
   
-  def recursive_function(gene_obj)
-    #puts @network.length
-    if @network.include? (gene_obj)
+  def recursive_function(gene_obj) #Recursive function that will look for the members of a network. 
+    if @network.include? (gene_obj) #if the network already contains the gene object, end the function. 
         return    
     end         
-    @network << gene_obj #mete el objeto gen en el atributo network que es una lista
-    gene_obj.interactions.each {|gene_int| #para cada interaccion (que es una string)
-        if @all_genes.has_key? (gene_int.downcase) #si ese gen es una key del diccionario con los genes objetos (es decir, si es un objeto Gen)
-            int_obj = @all_genes[gene_int.downcase] #dame ese objeto con sus respectivos atributos Gen1.2.(objeto)           
-            recursive_function(int_obj)
-        
-        end}      
-      
+    @network << gene_obj #store the Gene object in the attribute network (an array)
+    gene_obj.interactions.each {|gene_int| #for each interaction (gen)
+        if @all_genes.has_key? (gene_int.downcase) #if that gen is a key pf the hash with the gene objects:
+            int_obj = @all_genes[gene_int.downcase] #store that object in a variable           
+            recursive_function(int_obj) #apply the function again
+     
+        end}               
+     
   end
   
   def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
@@ -61,40 +61,39 @@ class InteractionNetwork
   end
   
   
-  #
-  def obtain_kegg_network
-    @network.each{|gene_id|
-      if gene_id.level.to_i == 1      
-        result_kegg_pathways = fetch("http://togows.org/entry/kegg-genes/ath:#{gene_id.agi_locus}/pathways.json")
-        kegg_pathways_json = JSON.parse(result_kegg_pathways) #transform the results into JSON format
-        if kegg_pathways_json == []
+  
+  def obtain_kegg_network #Function that store all the KEGG annotations of the original genes of the network.
+    @network.each{|gene_id| #for each gene object of the network
+      if gene_id.level.to_i == 1  #if the gene is a original gene (level 1)
+        result_kegg_pathways = fetch("http://togows.org/entry/kegg-genes/ath:#{gene_id.agi_locus}/pathways.json")#search for the KEGG annotations by the AGI locus. 
+        kegg_pathways_json = JSON.parse(result_kegg_pathways) #transform the results into a JSON format
+        if kegg_pathways_json == [] #if that gene hasn't KEGG annotations, skip to the next gene.
             next
         end
-        kegg_pathways_gene = kegg_pathways_json[0]  #store the hash with the KEGG ID and the patways' name
-        kegg_pathways_gene.each {|key,value| unless @kegg_annotations.has_key?(key)
-            @kegg_annotations[key] = value
+        kegg_pathways_gene = kegg_pathways_json[0] #store the array that contains the KEGG annotations 
+        kegg_pathways_gene.each {|key,value| unless @kegg_annotations.has_key?(key) #store the hash with the KEGG ID and the patways' name unless the hash has already that KEGG key. 
+            @kegg_annotations[key] = value #The key is the KEGG ID and the value is the KEGG pathway name.
         end}
       else
          next
       end}
   end
-  #
-  def obtain_go_network
-    @network.each{|gene_id|
-    if gene_id.level.to_i == 1  
-      result_go_pathways = fetch("http://togows.org/entry/ebi-uniprot/#{gene_id.agi_locus}/dr.json")
-      go_pathways_json = JSON.parse(result_go_pathways) #transform the results into JSON format
-      if go_pathways_json.empty?
+  
+  def obtain_go_network #Function that store all the GO annotations of the original genes of the network.
+    @network.each{|gene_id|#for each gene object of the network
+    if gene_id.level.to_i == 1 #if the gene is a original gene (level 1)
+      result_go_pathways = fetch("http://togows.org/entry/ebi-uniprot/#{gene_id.agi_locus}/dr.json")#search for the GO annotations by the AGI locus. 
+      go_pathways_json = JSON.parse(result_go_pathways) #transform the results into a JSON format
+      if go_pathways_json.empty? #if that gene does not have GO annotations, skip. 
          next
       else
-         #select only the biological process:
-         go_biological_process = go_pathways_json[0]["GO"].select!{|array| array.any?{|element| element =~ /P:/}}
-         go_biological_process.each{|result| @go_annotations[result[0]] = result[1]}
+         #select only the biological process of the GO annotations:
+         go_biological_process = go_pathways_json[0]["GO"].select!{|array| array.any?{|element| element =~ /P:/}} #select only those arrays that contain the biological processes
+         go_biological_process.each{|result| @go_annotations[result[0]] = result[1]} #the key will be the GO ID and the values will be the GO pathways. 
       end
      else
       next 
     end}
   end
-  #
-  
+    
 end
--- Assignment_2/main_code.rb
+++ Assignment_2/main_code.rb
@@ -1,12 +1,15 @@
-## main code
-file = ARGV[2]
+###### MAIN CODE
+
+file = ARGV[2] #save the file in a variable.
+
+#load the objects and tool that the script needs:
 require 'rest-client'
 require '/home/osboxes/BioinformaticsCourseGitHub/BioProCha_Assignments_LSG/Assignment_2/InteractionNetwork_obj.rb'
 require '/home/osboxes/BioinformaticsCourseGitHub/BioProCha_Assignments_LSG/Assignment_2/gene_obj.rb'
 
 
 
-#saving the genes of the list in an array:
+#Function that saves the genes of the list in an array:
 def read_file_genes(file)
   genes_list = Array.new
   lines = IO.readlines(file)
@@ -16,83 +19,99 @@ return genes_list
 end
 
 
-#### OBTAIN THE INTERACTIONS OF THE GENES FROM THE ORIGINAL LIST
-genes_list = read_file_genes(file)
-hash_interactions = Hash.new
+########## 1. OBTAIN THE INTERACTIONS OF THE GENES FROM THE ORIGINAL LIST
+genes_list = read_file_genes(file).sort #array that contains the lines of the file
+hash_interactions = Hash.new #hash that will contain the genes objects
+
+#each value of the hash is a Gene object that corresponds with a gene from the original list: 
 genes_list.each {|gene| 
-hash_interactions["#{gene.downcase}"] = Gene.new(:agi_locus => "#{gene.downcase}",:level => 1)} #each value of the hash is a Gene object that correspond with the genes from the original list
- 
+hash_interactions["#{gene.downcase}"] = Gene.new(:agi_locus => "#{gene.downcase}",:level => 1)} 
 
-#### STORE THE NEW GENES (Level 2) IN ANOTHER LIST, TRANSFORM THEM INTO A GENE OBJECT AND LOOK FOR THEIR INTERACTIONS
-genes_list2 = Array.new()
-hash_interactions.values.each {|gene_obj| gene_obj.interactions.each {|gene| genes_list2 << gene}}
+####### 1.2. STORE THE NEW GENES (Level 2) IN ANOTHER LIST, TRANSFORM THEM INTO A GENE OBJECT AND LOOK FOR THEIR INTERACTIONS
+genes_list2 = Array.new() #array that will contain the genes of level 2 (which correspond with the genes that interact with the original genes)
+hash_interactions.values.each {|gene_obj| gene_obj.interactions.each {|gene| genes_list2 << gene}} #store in the array the genes contained in the attribute interactions of genes level = 1.
 
-
+#store in the hash the new genes as Gene Object:
 genes_list2.each {|gene| 
-hash_interactions["#{gene.downcase}"] = Gene.new(:agi_locus => "#{gene.downcase}",:level => 2)} #each value of the hash is a Gene object that correspond with the genes from the original list
+hash_interactions["#{gene.downcase}"] = Gene.new(:agi_locus => "#{gene.downcase}",:level => 2)} 
 
-#### STORE THE NEW GENES (Level 3) IN ANOTHER LIST, TRANSFORM THEM INTO A GENE OBJECT AND LOOK FOR THEIR INTERACTIONS
-genes_list3 = Array.new()
-hash_interactions.values[168..-1].each {|gene_obj|
+####### 1.3. STORE THE NEW GENES (Level 3) IN ANOTHER LIST, TRANSFORM THEM INTO A GENE OBJECT AND LOOK FOR THEIR INTERACTIONS
+genes_list3 = Array.new()#array that will contain the genes of level 3 (which correspond with the genes that interact with genes of level 2)
+
+#store in the array the genes contained in the attribute interactions of genes level = 2:
+hash_interactions.values[168..-1].each {|gene_obj| 
   if gene_obj.level == 2
     gene_obj.interactions.each {|gene| genes_list3 << gene}
   else
     next
   end}
 
+#store in the hash the new genes as Gene Object:
 genes_list3.each {|gene|
-   if hash_interactions.has_key?(gene)
+   if hash_interactions.has_key?(gene) #if there is already a Gene object with the same AGI locus, skip to the next gene 
        next   
    else 
      hash_interactions["#{gene.downcase}"] = Gene.new(:agi_locus => "#{gene.downcase}",:level => 3)
-   end} #each value of the hash is a Gene object that correspond with the genes from the original list
+   end} 
 
 
-
-### DELETING GENES THAT DON'T HAVE INTERACTIONS:
+######### 2.1. DELETING GENES THAT DON'T HAVE INTERACTIONS:
  
 hash_interactions_updated = Hash.new #new hash that will contain only those genes that have interactions
-hash_interactions.values.each{|gene_obj|
-  if gene_obj.interactions.length < 1
+hash_interactions.values.each{|gene_obj| #each value of the hash is a Gene object.
+  if gene_obj.interactions.length < 1 #if that gene don't have any interactions, skip. 
     next
   end
-  if gene_obj.level > 1 and gene_obj.interactions.length < 2
+  if gene_obj.level > 1 and gene_obj.interactions.length < 2 #if it is a gene from level 2 or 3 and have 0 or 1 interaction, skip.
     next
   end
-  hash_interactions_updated[gene_obj.agi_locus] = gene_obj}
+  hash_interactions_updated[gene_obj.agi_locus] = gene_obj}#store the rest of the genes objects in the new hash. 
 
 
-######### CREATING AND OBTAINING INTERACTIONS NETWORKS USING THE INTERACTION NETWORK CLASS:
-list_of_networks = Array.new
-hash_interactions_updated.values.each {|gene|                            
-                            if list_of_networks.any?{|int_network| int_network.network.any?{|int| gene.agi_locus == int.agi_locus}} #para no hacer redes repetidas
+######### 2.2. SORTING THE HASH BY THE AGI_LOCUS (this will allows us after to compare the gene in networks in order to avoid networks with the same genes)
+sorted_keys_hash = Array.new(hash_interactions_updated.keys.sort)
+hash_updated_sorted = Hash.new
+
+sorted_keys_hash.each{|key| hash_updated_sorted[key] = hash_interactions_updated[key]}
+
+######### 3.CREATING AND OBTAINING INTERACTIONS NETWORKS USING THE INTERACTION NETWORK CLASS:
+list_of_networks = Array.new #array that contain will contain the networks found
+
+hash_updated_sorted.values.each {|gene|                            
+                            if list_of_networks.any?{|int_network| int_network.network.any?{|int| int.agi_locus.to_s == gene.agi_locus.to_s}} #para no hacer redes repetidas
                               next
                             else
-                              network = InteractionNetwork.new(:gene_original => gene,:all_genes => hash_interactions_updated)                               
+                              network = InteractionNetwork.new(:gene_original => gene,:all_genes => hash_updated_sorted)                               
                               list_of_networks << network
                             end}
 
-#### A FUNCTION THAT STORE IN AN ARRAY ONLY THE NETWORKS THAT INCLUDES AT LEAST TWO OF THE ORIGINAL GENES:
+######### 4. A FUNCTION THAT STORE IN AN ARRAY ONLY THE NETWORKS THAT INCLUDES AT LEAST TWO OF THE ORIGINAL GENES:
 def remove_networks_no_functional(array_of_networks,new_list_networks)
-  array_of_networks.each {|int_network_obj| int_network = int_network_obj.network 
-    if int_network.count{|gene| gene.level.to_i == 1} < 2
+  array_of_networks.each {|int_network_obj| int_network = int_network_obj.network #store the network (array) in a variable
+    if int_network.count{|gene| gene.level.to_i == 1} < 2 #if there is less than two of the original genes in the network, skip to the another network.
        next
     else
-       new_list_networks << int_network_obj
+       new_list_networks << int_network_obj #store the network in the new list 
     end}
   return new_list_networks
 end
 
 list_networks_updated = remove_networks_no_functional(list_of_networks,Array.new) #applies the function to the list of total networks obtained
-puts "KEGG ANNOTATIONS: "
-list_networks_updated.each{|net| puts net.kegg_annotations
- puts
- puts
- }
-puts "GO ANNOTATIONS: "
-list_networks_updated.each{|net| puts net.go_annotations
- puts
- puts
- } 
- 
-    
+
+
+######### 5. CREATING REPORT:
+report = File.open("report","w")
+report << "REPORT: IDENTIFICATION OF INTERACTION NETWORKS\n\n" #header
+report << "#{list_networks_updated.length} networks have been found using a value of IntAct miscore of 0.45.\n"
+list_networks_updated.each {|net| report << "\n\n\nNetwork: Contains #{net.network.length} genes in total\n"
+   report << "The original genes that participe in this network are: \n"
+   net.network.each{|gene|
+    if gene.level.to_i == 1
+      report << gene.agi_locus.upcase.to_s + "\n"
+    end}
+   report << "\n" + "The KEGG annotations of the original genes involved in this network are:\n\n"
+   net.kegg_annotations.each {|key,value| report << "KEGG ID: " + key.to_s + "\t\t" + "Pathway name: " + value.to_s + "\n"}
+   report << "\n\n" + "The GO annotations of the original genes involved in this network are:\n"
+   net.go_annotations.each {|key,value| report << "GO ID: " + key.to_s + "\t\t" + "Pathway name: " + value.to_s + "\n"}}
+
+report.close
